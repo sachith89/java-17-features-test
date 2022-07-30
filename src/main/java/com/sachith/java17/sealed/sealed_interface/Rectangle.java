993e6c678cf1796f4ec1b21dc6eac9734d48d9eb
Sachith <sachithlakmal@icloud.com>
2022-07-30 11:07:52 +0530

    sealed interface

---
+++ src/main/java/com/sachith/java17/sealed/sealed_interface/Rectangle.java
@@ -0,0 +1,4 @@
+package com.sachith.java17.sealed.sealed_interface;
+
+public final class Rectangle implements Shape{
+}
---
+++ src/main/java/com/sachith/java17/sealed/sealed_interface/Shape.java
@@ -0,0 +1,4 @@
+package com.sachith.java17.sealed.sealed_interface;
+
+public sealed interface Shape permits Circle, Square, Rectangle{
+}
